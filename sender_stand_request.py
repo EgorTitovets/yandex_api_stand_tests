0754328f0ea6bab567759c76bcb6c4a4a6f7915f
Egor <pryanik0992@gmail.com>
2024-07-24 16:57:37 +0500

    Revert "Рефакторинг кода в Sendder_stand_requast.py"
    This reverts commit dbfb27393c90296ec8cdfaa7d94b66f175db3e89.

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -15,4 +15,19 @@ print(response.json())
 
 def get_users_table():
     return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+#
+# response = get_users_table()
+#
+# print(response.status_code)
 
+
+# def post_products_kits(products_ids):
+#     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+#                          json=products_ids,
+#                          headers=data.headers)
+#
+#
+# response = post_products_kits(data.product_ids);
+
+# print(response.status_code)
+# print(response.json())
